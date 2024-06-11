82b7b947a0b5a736eccc9de83f57212f1dc4dbf8
Thiago Muniz <muniz@duck.com>
2024-06-11 14:45:21 -0400

    removed force dinamic

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -5,9 +5,7 @@ import { CreateInvoice } from '@/app/ui/invoices/buttons';
 import { lusitana } from '@/app/ui/fonts';
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import { Suspense } from 'react';
-
-export const dynamic = 'force-dynamic';
-
+ 
 export default async function Page({
     searchParams,
   }: {
